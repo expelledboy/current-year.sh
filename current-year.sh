2ec575a33612aa1d8af349e3896ca8550797d5dc
expelledboy <102334+expelledboy@users.noreply.github.com>
2023-12-09 22:49:43 +0200

    Make current year valid

--- current-year.sh
+++ current-year.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-eval "$(brush "v0.0.2")"
-
-unset -f timestamp
-
 timestamp() (
 	date +"%Y-%m-%d %H:%M:%S"
 )
 
-
-log() (
-	echo "$(timestamp) $@"
+current_year() (
+	timestamp | awk -F'-' '{print $1}'
 )
